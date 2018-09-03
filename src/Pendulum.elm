40e5249573015b7e63b4cc1276f0c870a29da9bc
Mark Williams <markrd.williams@gmail.com>
2018-09-03 12:13:53 +0100

    continue Pendulum and Point

--- src/Pendulum.elm
+++ src/Pendulum.elm
@@ -15,7 +15,49 @@ type alias Pendulum =
     , drivingFreq : Float
     }
 
-renderPendulum : Pendulum -> Svg Msg
-renderPendulum pendulum =
+calcThetadotdot : Pendulum -> Float -> Float
+calcThetadotdot { theta, thetadot, thetadotdot
+                , len, damping, driving, drivingFreq 
+                } time =
+    let 
+        accelerationMass = ((-9.8/100) / len) * sin theta
+        accelerationDamping = damping * thetadot
+        accelerationDamping = driving * 100 / len * cos (drivingFreq * time)
+
+rungeKutta : (Float -> Float -> Float) 
+           -> Float -> Point number 
+           -> Point number
+rungeKutta f h (Point x y) =
     let
+        k1 = h * (f x y)
+        k2 = h * (f (x + h/2) (y + k1/2))
+        k3 = h * (f (x + h/2) (y + k2/2))
+        k4 = h * (f (x + h) (y + k3))
+    in
+        Point (x + h) 
+              (y + (k1 + 2 * k2 + 2 * k3 + k4)/6)
+
+
+
+renderPendulum : Pendulum -> List (Svg Msg)
+renderPendulum {theta, len, pivotLocation} =
+    let
+        coords = (Point.fromPolarCoords (theta, len)) + pivotLocation
+    in
+        [ circle 
+            [ cx (toString (x coords))
+            , cy (toString (y coords))
+            , r "20"
+            , stroke "black"
+            , fill "black"
+            ]
+        , line 
+            [ 
+            ]
+        ]
+
+
+        
+
+
         
--- src/Point.elm
+++ src/Point.elm
@@ -5,7 +5,17 @@ type Point number  = Point number number
 fromCoords : ( number, number ) -> Point number
 fromCoords (x, y) = Point x y 
 
-fromPolarCoords : ( Float, Float ) -> Point Float 
-fromPolarCoords polar = fromCoords (fromPolar polar)
+fromPolar : ( Float, Float ) -> Point Float 
+fromPolar polar = fromCoords (fromPolar polar)
 
+toPolar : Point number -> (number, number)
+toPolar (Point x y) = toPolar (x, y)
 
+(+) : Point number -> Point number -> Point number
+(+) (Point x1 y1) (Point x2 y2) = Point (x1 + y1) (x2 + y2)
+
+x : Point number -> number
+x (Point x _) = x
+
+y : Point number -> number
+y (Point _ y) = y
